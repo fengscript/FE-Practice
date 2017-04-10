9f9591b0212052c8e75696f5dc00ed22f4f1312b
fengyanggang <fengyanggang@hotmail.com>
2017-04-10 17:23:58 +0800

    *

---
+++ ng/hello-angular/src/app/flogin/flogin.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flogin',
+  template: `
+    <p>
+      flogin Works!
+    </p>
+  `,
+  styles: []
+})
+export class FloginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

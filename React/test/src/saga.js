1865d914be27d1c295d071b546efd01411c9232c
fengyanggang <fengyanggang@hotmail.com>
2019-07-07 15:36:44 +0800

    add saga notes

---
+++ React/test/src/saga.js
@@ -0,0 +1,13 @@
+import { call, put, takeEvery, takeLatest } from "redux-saga/effects";
+
+const getMsg = async () => {
+    await setTimeout(() => {
+        console.log('start fetch msg');
+    }, 2000)
+    
+    setTimeout(() => {
+        console.log('no await');
+    }, 1000)
+};
+
+getMsg()
--- React/test/src/store.js
+++ React/test/src/store.js
@@ -16,10 +16,11 @@ const actions = {
   ADD_NUMBER
 };
 
-
 const reducer = (state = initState, action) => {
+  // const count = state.count;
   switch (action.type) {
     case "ADD_NUMBER":
+      // return { count: count + 1 };
       return { ...state, count: state.count + action.payload };
     default:
       return state;

cd5cf9cbd8cc92a49c8a39b8dab619697c9414bb
fengyanggang <fengyanggang@hotmail.com>
2019-08-04 00:00:27 +0800

    add saga notes

---
+++ React/test/src/mockData.js
@@ -0,0 +1,5 @@
+export default {
+  fyg: "123",
+  remote: "https://my-json-server.typicode.com/typicode/demo/posts",
+  back: "https://my-json-server.typicode.com/typicode/demo"
+};
--- React/test/src/saga.js
+++ React/test/src/saga.js
@@ -1,3 +1,6 @@
+// url
+// https://my-json-server.typicode.com/typicode/demo/posts
+
 import {
   call,
   put,
@@ -8,27 +11,49 @@ import {
   takeLatest
 } from "redux-saga/effects";
 import { selectors } from "./store";
+import _data from "./mockData";
 
 const delay = second => new Promise(res => setTimeout(res, second * 1000));
 
+async function fetchData(url) {
+  const response = await fetch(url);
+  return response.json();
+}
+
 export function* increaseAsync() {
   const getCount = yield select();
-  yield delay(1);
-  console.log("saga in");
-  console.log(getCount.count);
+  yield fetchData(_data.remote).then(res => {
+    
+    const data = JSON.stringify(res);
+
+    console.log(data);
+  });
+  // yield delay(1);
+  // console.log(getCount.count);
+
   //   yield put({
   //     type: "INCREASE_ASYNC",
   //     payload: 1
   //   });
+
+  console.log("data end");
 }
 // INCREASE_ASYNC
 export function* watchIncreasementAsync() {
   yield takeEvery("INCREASE_ASYNC", increaseAsync);
 }
 
-function* helloSaga() {
+function helloSaga() {
   console.log("Hello Sagas!");
 }
+
+// function fetchJson(url) {
+//   return fetch("http://localhost:3333/person").then(response =>
+//     response.json()
+//   );
+// }
+
+// fetchData().then(data => console.log(JSON.stringify(data)));
 
 export default function* rootSaga() {
   yield all([helloSaga(), watchIncreasementAsync()]);
